74b70d03c9d2eb3b59c1df084b02da479f576eee
DanielleSmith0 <daniellesmith@Danielles-MacBook-Pro.local>
2023-03-13 22:35:57 -0600

    invoice continued

--- invoice.sql
+++ invoice.sql
@@ -1,4 +1,16 @@
+1.
+SELECT COUNT(*) FROM invoice
+WHERE billing_country = 'USA'
 
-
+2.
+SELECT MAX(total) FROM invoice
 -- How to delete invoive with invoice id of 1
 -- the id is being referenced in another table, so it needed to be deleted in both locations. in line and in table. Foreign key.
+
+3.
+SELECT MIN(total) FROM invoice
+
+4.
+SELECT * FROM invoice
+WHERE total > 5 
+
